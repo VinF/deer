93fc68d22fe91f2a4c6ecbd2ca3346be9ee63b6a
David Taralla <davidtaralla@gmail.com>
2016-02-05 10:34:53 +0100

    A bit of renaming according to new conventions

--- General_Deep_Q_RL/data_set.py
+++ General_Deep_Q_RL/data_set.py
@@ -13,7 +13,7 @@ class DataSet(object):
     """A replay memory consisting of circular buffers for observations, actions, and rewards.
 
     """
-    def __init__(self, history_sizes, randomState, maxSize=1000):
+    def __init__(self, history_sizes, randomState=None, maxSize=1000):
         """Construct a DataSet.
 
         Arguments:
@@ -25,8 +25,8 @@ class DataSet(object):
         self._historySizes = history_sizes
         self._maxHistorySize = np.max(history_sizes)
         self._size = size
-        self._observations = np.zeros(size, dtype='object')
-        self._actions      = np.zeros(size, dtype='int32')
+        self._observations = np.zeros(len(history_sizes), dtype='object') # One list per input; will be initialized at 
+        self._actions      = np.zeros(size, dtype='int32')                # first call of addState
         self._rewards      = np.zeros(size, dtype=theano.config.floatX)
         self._terminals    = np.zeros(size, dtype='bool')
 
@@ -36,50 +36,79 @@ class DataSet(object):
             self._randomState = randomState
 
         self._nElems  = 0
-        self._current = 0
 
 
     def randomBatch(self, batch_size):
-        """Return corresponding states, actions, rewards, terminal status, and next_states for batch_size randomly chosen state transitions.
-        Note that if terminal[-1] == True, then next_states[-1] == None.
+        """Return corresponding states, actions, rewards, terminal status, and next_states for batch_size randomly 
+        chosen state transitions. Note that if terminal[i] == True, then 
+        next_states[input][i] == np.zeros_like(states[input][i]) for all 'input's.
         
         Arguments:
-            batch_size - Number of elements in the batch
+            batch_size - Number of elements in the batch.
 
         Returns:
-            states - A list of size batch_size, where each element is an ndarray(dtype='object') 'A' of size 'number of inputs'. For each input i, 
-                     A[i] is a 3D matrix where first dimension denotes the history index (if no history, = 1), and the 
-                     two others can be used to represent actual data.
+            states - An ndarray(size=number_of_inputs, dtype='object), where states[input] is a 2+D matrix of dimensions
+                     batch_size x input.historySize x "shape of a given ponctual observation for this input". States were
+                     taken randomly in the data set such that they are complete regarding the histories of each input.
             actions - The actions taken in each of those states.
             rewards - The rewards obtained for taking these actions in those states.
-            next_states - Same than states, but shifted to the left by 1 time unit. If terminal[-1] == True, then next_states[-1] == None.
-            terminal - Whether these actions lead to terminal states.
+            next_states - Same structure than states, but next_states[i][j] is guaranteed to be the information 
+                          concerning the state following the one described by states[i][j] for input i.
+            terminals - Whether these actions lead to terminal states.
 
+        Throws:
+            SliceError - If a batch of this size could not be built based on current data set (not enough data or 
+                         all trajectories are too short).
         """
-        start, end = _randomSlice(batch_size + self._maxHistorySize)
-        start += self._maxHistorySize
-        if (end < start):
-            end += self._nElems
+
+        rndValidIndices = np.zeros(batch_size)
+        for i in range(batch_size): # TODO: multithread this loop?
+            rndValidIndices[i] = _randomValidStateIndex()
+            
+        
+        actions   = self._actions[rndValidIndices]
+        rewards   = self._rewards[rndValidIndices]
+        terminals = self._terminals[rndValidIndices]
+        states = np.zeros(len(history_sizes), dtype='object')
+        next_states = np.zeros_like(states)
+
+        for input in range(len(_historySizes)):
+            states[input] = np.zeros((batch_size, self._historySizes[input],) + np.array(state[i]).shape)
+            for i in range(batch_size):
+                states[input][i] = self._observations[input][rndValidIndices[i]-self._historySizes[input]:rndValidIndices[i]]
+                if rndValidIndices[i] >= self._nElems - 1 or terminals[i]:
+                    next_states[input][i] = np.zeros_like(states[input][i])
+                else:
+                    next_states[input][i] = self._observations[input][rndValidIndices[i]+1-self._historySizes[input]:rndValidIndices[i]+1]
 
         
-        actions   = self._actions.take(range(start, end), mode='wrap')
-        rewards   = self._rewards.take(range(start, end), mode='wrap')
-        terminals = self._terminals.take(range(start, end), mode='wrap')
-
-        states = [None] * batch_size
-        for i in range(batch_size):
-            states[i] = np.zeros(len(self._historySizes), dtype='object')
-            for j in range(len(self._historySizes)):
-                states[i][j] = self._observations.take(range(start+i-self._historySizes[j], end), mode='wrap')
-                
-        next_states = [None] * batch_size
-        next_states[0:-1] = states[1:]
-        if (terminals[-1] == False):
-            next_states[-1] = np.zeros(len(self._historySizes), dtype='object')
-            for j in range(len(self._historySizes)):
-                next_states[-1][j] = self._observations.take(range(end+1-self._historySizes[j], end+1), mode='wrap')
-
         return states, actions, rewards, next_states, terminals
+
+    def _randomValidStateIndex(self):
+        index = self._randomState.randint(self._maxHistorySize-1, self._nElems)
+        
+        # Check if slice is valid wrt terminals
+        firsTry = index
+        startWrapped = False
+        while True:
+            i = index-1
+            for processed in range(1, self._maxHistorySize):
+                if (i < 0 or self._terminals[i]):
+                    break;
+
+                i -= 1
+            
+            if (processed < self._maxHistorySize - 1):
+                # if we stopped prematurely, shift slice to the left and try again
+                index = i
+                if (index < 0):
+                    startWrapped = True
+                    index = self._nElems - 1
+                if (startWrapped and start <= firstTry):
+                    raise SliceError("Could not find a state with full histories")
+            else:
+                # else index was ok according to terminals
+                return index
        
 
     def nElems(self):
@@ -90,18 +119,26 @@ class DataSet(object):
         return self._nElems
 
 
-    def addObservation(self, observation):
+    def addPonctualObservation(self, ponctualObs):
         """Store an observation in the dataset. If the buffer is full, it uses a least recently used (LRU) approach 
         to replace an old element by the new one. Note that two subsequent calls to this function is prohibited; they
         should be interleaved with a call to addActionRewardTerminal.
 
         Arguments:
-            observation - An ndarray(dtype='object') whose length is the number of inputs.
-                          For each input i, observation[i] is a 2D matrix that represents the actual data.
+            ponctualObs - An ndarray(dtype='object') whose length is the number of inputs.
+                          For each input i, ponctualObs[i] is a 2D matrix that represents the actual data.
 
         """
+
+        # Initialize the observations container if necessary
+        if (self._nElems == 0):
+            for i in range(len(self._historySizes)):
+                self._observations[i] = np.zeros((self._size,) + np.array(ponctualObs[i]).shape)
         
-        self._observations[self._current] = observation
+        # Store observations
+        for i in range(len(self._historySizes)):
+            self._observations[i] = np.roll(self._observations[i], -1)
+            self._observations[i][-1] = ponctualObs[i]
 
 
     def addActionRewardTerminal(self, action, reward, isTerminal):
@@ -116,37 +153,37 @@ class DataSet(object):
                          isTerminal) marks the end of a trajectory).
 
         """
-        self._actions[self._current] = action;
-        self._rewards[self._current] = reward;
-        self._terminals[self._current] = isTerminal;
+        self._actions = np.roll(self._actions, -1)
+        self._rewards = np.roll(self._rewards, -1)
+        self._terminals = np.roll(self._terminals, -1)
+        self._actions[-1] = action;
+        self._rewards[-1] = reward;
+        self._terminals[-1] = isTerminal;
 
-        self._current += 1
-        if (self._current >= self._size):
-            self._current = 0
         if (self._nElems < self._size):
             self._nElems += 1
 
 
-    def addSample(self, observation, action, reward, isTerminal):
-        """Store a (observation, action, reward, isTerminal) in the dataset. 
-        Equivalent to 'addState(observation); addActionRewardTerminal(action, reward, isTerminal)'.
+    def addSample(self, ponctualObs, action, reward, isTerminal):
+        """Store a (state, action, reward, isTerminal) in the dataset. 
+        Equivalent to 'addState(state); addActionRewardTerminal(action, reward, isTerminal)'.
 
         Arguments:
-            observation - An ndarray(dtype='object') whose length is the number of inputs.
-                          For each input (so for each i in 0 <= i < M), observation[i] is a 2D matrix that represents 
-                          actual data.
+            ponctualObs - An ndarray(dtype='object') whose length is the number of inputs.
+                          For each input i, observation[i] is a 2D matrix that represents actual data.
             action - The id of the action taken in the last inserted state using addState.
             reward - The reward associated to taking 'action' in the last inserted state using addState.
             isTerminal - Tells whether 'action' lead to a terminal state (i.e. whether the tuple (state, action, reward, isTerminal) marks the end of a trajectory).
 
         """
-        addState(observation)
+        addState(ponctualObs)
         addActionRewardTerminal(action, reward, isTerminal)
         
 
     def _randomSlice(self, size):
-        """Get start and end indices of a random contiguous batch from the dataset. Two objects are considered "contiguous" if
-        1) they are next to each other in the dataset, i.e. at position i and (i+1) % size
+        """Get start and end indices of a random contiguous batch from the dataset. Two objects at i and j are 
+        considered "contiguous" if
+        1) they are next to each other in the dataset, i.e. at position i and j=i+1
         2) self._terminals[i] == False.
 
         Arguments:
@@ -158,11 +195,11 @@ class DataSet(object):
 
         slice = np.zeros(size, dtype=_buffer.dtype)
 
-        start = self._randomState.randint(0, self._nElems)
+        start = self._randomState.randint(0, self._nElems - size)
         end   = start + size
         
-        # Check if slice is valid wrt mask
-        firstTry = start
+        # Check if slice is valid wrt terminals
+        firsTry = start
         startWrapped = False
         while True:
             i = start
@@ -180,12 +217,13 @@ class DataSet(object):
                 start = end - size
                 if (start < 0):
                     startWrapped = True
-                    start = self._nElems + start
+                    end = self._nElems
+                    start = end - size
                 if (startWrapped and start <= firstTry):
                     raise SliceError("Could not find a slice of size " + size)
             else:
                 # else slice was ok according to mask
-                return start, end # self._buffer.take(range(start, end), mode='wrap')
+                return start, end
 
         
 class SliceError(LookupError):
