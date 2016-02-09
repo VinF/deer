28008afc9e2cfa7d93cdfae2810087eff4f8983c
David Taralla <davidtaralla@gmail.com>
2016-02-09 15:40:33 +0100

    Fix import remnant

--- General_Deep_Q_RL/agent.py
+++ General_Deep_Q_RL/agent.py
@@ -101,7 +101,7 @@ class NeuralAgent(object):
             states, actions, rewards, next_states, terminals = self._dataSet.randomBatch(self._batchSize)
             loss = self._network.train(states, actions, rewards, next_states, terminals)
             self._trainingLossAverages.append(loss)
-        except data_set.SliceError as e:
+        except SliceError as e:
             warn("Training not done - " + e, AgentWarning)
 
     def run(self, nEpochs, epochLength):
