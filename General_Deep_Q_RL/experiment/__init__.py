eab5269ba6dc88db00ce32d94f141ac705f46a54
David Taralla <davidtaralla@gmail.com>
2016-03-02 12:10:38 +0100

    Fix line endings (once for all?)

--- General_Deep_Q_RL/experiment/__init__.py
+++ General_Deep_Q_RL/experiment/__init__.py
@@ -1,4 +1,4 @@
-"""This module contains all predefined controllers for building experiments in the General Deep Q-Learning framework.
-
-Authors: Vincent Francois-Lavet, David Taralla
-"""
+"""This module contains all predefined controllers for building experiments in the General Deep Q-Learning framework.
+
+Authors: Vincent Francois-Lavet, David Taralla
+"""
