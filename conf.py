e1200475d8936b1537d89aab2fd6652b0269a636
Vincent Francois <vincent.francois@gmail.com>
2016-04-07 14:13:19 +0200

    Fix readthedocs C dependencies

--- conf.py
+++
@@ -1,5 +0,0 @@
-import mock
- 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
-for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 import mock
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'theano', 'joblib']
 for mod_name in MOCK_MODULES:
 sys.modules[mod_name] = mock.Mock()
 
