a7a4e12d29d0275c67310f88912cc4cb9052b178
Arnaud Joly <arnaud.v.joly@gmail.com>
2016-04-15 13:36:45 +0200

    fix import

--- ci_scripts/install.sh
+++ ci_scripts/install.sh
@@ -26,7 +26,7 @@ popd
 # provided versions
 conda create -n testenv --yes python=$PYTHON_VERSION pip nose \
       numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION \
-      matplotlib theano
+      matplotlib theano joblib
 
 source activate testenv
 
