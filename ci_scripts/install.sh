3aababe4f2e7322b5d30a4a29d75bca43434b6e4
Arnaud Joly <arnaud4567@gmail.com>
2016-04-15 14:22:49 +0200

    Travis run example (#11)
    * improve travis
    
    * fix bug (1)?
    
    * fix path issue
    
    * fix bash syntax
    
    * fix path (2)
    
    * fix path issue (3)
    
    * fix path 3
    
    * fix import
    
    * fix package
    
    * fix theano install
    
    * fix package theano (2)
    
    * latest miniconda?
    
    * raise min numpy
    
    * update test matrix
    
    * reduce number of epochs
    
    * verbose test
    
    * deactivate coverage temp
    
    * add where the original script comes from

---
+++ ci_scripts/install.sh
@@ -0,0 +1,45 @@
+# inspired from scikit-learn contrib
+
+# Deactivate the travis-provided virtual environment and setup a
+# conda-based environment instead
+deactivate
+
+# Use the miniconda installer for faster download / install of conda
+# itself
+pushd .
+cd
+mkdir -p download
+cd download
+echo "Cached in $HOME/download :"
+ls -l
+echo
+if [[ ! -f miniconda.sh ]]
+   then
+   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+       -O miniconda.sh
+   fi
+chmod +x miniconda.sh && ./miniconda.sh -b
+cd ..
+export PATH=/home/travis/miniconda/bin:$PATH
+conda update --yes conda
+popd
+
+# Configure the conda environment and put it in the path using the
+# provided versions
+conda create -n testenv --yes python=$PYTHON_VERSION pip nose \
+      numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION \
+      matplotlib theano=$THEANO_VERSION joblib
+
+source activate testenv
+
+
+if [[ "$COVERAGE" == "true" ]]; then
+    pip install coverage coveralls
+fi
+
+python --version
+python -c "import numpy; print('numpy %s' % numpy.__version__)"
+python -c "import scipy; print('scipy %s' % scipy.__version__)"
+python -c "import theano; print('theano %s' % theano.__version__)"
+
+python setup.py develop
---
+++ ci_scripts/test.sh
@@ -0,0 +1,22 @@
+# inspired from scikit-learn contrib
+
+set -e
+
+if [[ "$EXAMPLE" == "toy_env" ]]; then
+    cd examples/toy_env
+    python run_toy_env.py --epochs 5
+
+else
+    # Get into a temp directory to run test from the installed and
+    # check if we do not leave artifacts
+    mkdir -p $TEST_DIR
+
+    cd $TEST_DIR
+
+    if [[ "$COVERAGE" == "true" ]]; then
+        nosetests -vs --with-coverage --cover-package=$MODULE $MODULE
+    else
+        nosetests -vs $MODULE
+    fi
+
+fi
