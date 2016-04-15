a3816469409afa82ff92c0d61b93fd1729bbe56b
Arnaud Joly <arnaud.v.joly@gmail.com>
2016-04-15 13:34:41 +0200

    fix path 3

--- ci_scripts/test.sh
+++ ci_scripts/test.sh
@@ -1,22 +1,24 @@
 set -e
 
-# Get into a temp directory to run test from the installed scikit learn and
-# check if we do not leave artifacts
-mkdir -p $TEST_DIR
 
-cd $TEST_DIR
-
-if [[ "$COVERAGE" == "true" ]]; then
-    nosetests -s --with-coverage --cover-package=$MODULE $MODULE
-else
-    nosetests -s $MODULE
-fi
-
-
-cd ..
 pwd
+ls
 
 if [[ "$EXAMPLE" == "toy_env" ]]; then
     cd examples/toy_env
     python run_toy_env.py
+
+else
+    # Get into a temp directory to run test from the installed and
+    # check if we do not leave artifacts
+    mkdir -p $TEST_DIR
+
+    cd $TEST_DIR
+
+    if [[ "$COVERAGE" == "true" ]]; then
+        nosetests -s --with-coverage --cover-package=$MODULE $MODULE
+    else
+        nosetests -s $MODULE
+    fi
+
 fi
