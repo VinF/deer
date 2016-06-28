977b50890172459ec519d76b3bfa5237695260a3
Vincent Francois <vincent.francois@gmail.com>
2016-06-28 16:56:58 +0200

    Adding LSTM possibility

---
+++ deer/q_networks/NN_keras_LSTM.py
@@ -0,0 +1,113 @@
+"""
+Neural network using LSTM's with Keras (called by q_net_keras)
+
+.. Author: Vincent Francois-Lavet
+"""
+
+import numpy as np
+from keras.models import Model
+from keras.layers import Input, Layer, Dense, Flatten, merge, Activation, Convolution2D, MaxPooling2D, Reshape
+from keras.layers.recurrent import LSTM
+import theano.tensor as T
+
+class NN():
+    """
+    Deep Q-learning network using Keras
+    
+    Parameters
+    -----------
+    batch_size : int
+        Number of tuples taken into account for each iteration of gradient descent
+    input_dimensions :
+    n_actions :
+    random_state : numpy random number generator
+    """
+    def __init__(self, batch_size, input_dimensions, n_actions, random_state):
+        self._input_dimensions=input_dimensions
+        self._batch_size=batch_size
+        self._random_state=random_state
+        self._n_actions=n_actions
+
+    def _buildDQN(self):
+        """
+        Build a network consistent with each type of inputs
+        """
+        layers=[]
+        outs_conv=[]
+        inputs=[]
+
+        for i, dim in enumerate(self._input_dimensions):
+            # - observation[i] is a FRAME
+            if len(dim) == 3:
+                input = Input(shape=(dim[0],dim[1],dim[2]))
+                inputs.append(input)
+                x = Convolution2D(32, 8, 8, border_mode='valid')(input)
+                x = MaxPooling2D(pool_size=(4, 4), strides=None, border_mode='valid')(x)
+                x = Convolution2D(64, 4, 4, border_mode='valid')(x)
+                x = MaxPooling2D(pool_size=(2, 2), strides=None, border_mode='valid')(x)
+                x = Convolution2D(64, 3, 3)(x)
+                
+                # We may add here LSTM's after having flatten the last two dimensions
+                
+                x = Flatten()(x)           
+
+            # - observation[i] is a VECTOR
+            if len(dim) == 2:
+                input = Input(shape=(dim[0],dim[1]))
+                inputs.append(input)
+                
+                if dim[0] > 3:
+
+                    x = LSTM(16,
+                        activation='relu',
+                        return_sequences=True)(input)
+                    x = LSTM(16,
+                        activation='relu',
+                        return_sequences=False)(x) # Structure many-to-one
+
+                else:
+                    x=input
+                    x = Flatten()(x)
+
+            # - observation[i] is a SCALAR -
+            elif(len(dim) == 1):
+            
+                input = Input(shape=(dim[0],))
+                inputs.append(input)
+                input = Reshape((dim[0],1))(input)
+                
+                if dim[0] > 3:                    
+                    x = LSTM(8,
+                            activation='relu',
+                            return_sequences=True)(input)
+                    x = LSTM(8,
+                            activation='relu',
+                            return_sequences=False)(x) # Structure many-to-one
+                else:
+                    x=input
+                    x = Flatten()(x)
+                            
+            outs_conv.append(x)
+        
+        if len(outs_conv)>1:
+            x = merge(outs_conv, mode='concat')
+        else:
+            x= outs_conv [0]
+                    
+        x = Dense(50, activation='relu')(x)
+        x = Dense(20, activation='relu')(x)
+        out = Dense(self._n_actions)(x)
+
+        model = Model(input=inputs, output=out)
+        layers=model.layers
+
+        # Grab all the parameters together.
+        params = [ param
+                    for layer in layers 
+                    for param in layer.trainable_weights ]
+
+        return model, params
+
+if __name__ == '__main__':
+    pass
+
--- docs/user/environments/toy_env_time_series.rst
+++ docs/user/environments/toy_env_time_series.rst
@@ -64,13 +64,13 @@ Navigate to the folder ``examples/toy_env/`` in a terminal window. The example c
 
     python run_toy_env_simple.py
 
-You can also choose the full version of the launcher that allows to specify the hyperparameters instead of choosing the default ones.
+You can also choose the full version of the launcher that specifies the hyperparameters for better performance.
 
 .. code-block:: bash
 
     python run_toy_env.py
 
-Every 10 epochs, a graph is saved in the 'toy_env' folder. You can then see that kind of behaviour for the test policy:
+Every 10 epochs, a graph is saved in the 'toy_env' folder. You can then see that kind of behaviour for the test policy at the end of the training:
 
 .. image:: http://vincent.francois-l.be/img_GeneralDeepQRL/plot_toy_example.png
    :width: 250 px
@@ -79,3 +79,37 @@ Every 10 epochs, a graph is saved in the 'toy_env' folder. You can then see that
 
 
 In this graph, you can see that the agent has successfully learned to take advantage of the price pattern to buy when it is low and to sell when it is high. This example is of course easy due to the fact that the patterns are very systematic which allows the agent to successfuly learn it. It is important to note that the results shown are made on a validation set that is different from the training and we can see that learning generalizes well. For instance, the action of buying at time step 7 and 16 is the expected result because in average this will allow to make profit since the agent has no information on the future.
+
+Using Convolutions VS LSTM's
+############################
+
+So far, the neural network was build by using a convolutional architecture as follows:
+
+.. image:: http://vincent.francois-l.be/img_GeneralDeepQRL/Convolutions_architecture.png
+   :width: 350 px
+   :alt: Convolutions architecture
+   :align: center
+
+The neural nework processes time series thanks to a set of convolutions layers. The output of the convolutions as well as the other inputs are followed by fully connected layers and the ouput layer.
+
+
+When working with deep reinforcement learning, it is also possible to work with LSTM's (see for instance this |introduction-LSTM|)
+
+.. |introduction-LSTM| raw:: html
+
+   <a href="http://colah.github.io/posts/2015-08-Understanding-LSTMs/" target="_blank">introduction to LSTM's</a>
+
+If you want to use LSTM's architecture, you can import the following libraries
+
+.. code-block:: bash
+
+    from deer.q_networks.q_net_keras import MyQNetwork
+    from deer.q_networks.NN_keras_LSTM import NN as NN_keras
+
+and then instanciate the qnetwork by specifying the 'neural_network' as follows:
+
+.. code-block:: bash
+
+     qnetwork = MyQNetwork(
+         env,
+         neural_network=NN_keras)
