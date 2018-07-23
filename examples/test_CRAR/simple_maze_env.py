e0f3c8a020f58d3d6278c1dbb6c5d6902d7c6dd4
Vincent Francois <vincent.francois@gmail.com>
2018-07-23 15:58:48 -0400

    clean simple_maze_env

--- examples/test_CRAR/simple_maze_env.py
+++ examples/test_CRAR/simple_maze_env.py
@@ -36,27 +36,14 @@ class MyEnv(Environment):
         self._map[:,-1]=1
         self._map[:,self._size_maze//2]=1
         self._map[self._size_maze//2,self._size_maze//2]=0
-        #self._map[:,self._size_maze//3]=1
-        #self._map[-2,self._size_maze//3]=0
-        #self._map[:,2*self._size_maze//3]=1
-        #self._map[2,2*self._size_maze//3]=0
         self._pos_agent=[2,2]
         self._pos_goal=[self._size_maze-2,self._size_maze-2]
-        #self._map[3,6]=0.66
 
                 
     def reset(self, mode):
         self.create_map()
 
         self._map[self._size_maze//2,self._size_maze//2]=0
-        #if mode == -1:
-        #    i=np.random.randint(2)
-        #    if(i==0):
-        #        self._map[self._size_maze//2-1,self._size_maze//2]=0
-        #    if(i==1):
-        #        self._map[self._size_maze//2+1,self._size_maze//2]=0
-        #else:
-        #    self._map[self._size_maze//2+1,self._size_maze//2]=0
         
         if mode == MyEnv.VALIDATION_MODE:
             if self._mode != MyEnv.VALIDATION_MODE:
@@ -69,10 +56,8 @@ class MyEnv(Environment):
         elif self._mode != -1:
             self._mode = -1
         
-        #if self._mode == -1:
+        # Setting the starting position of the agent
         self._pos_agent=[self._size_maze//2,self._size_maze//2]
-        #else:
-        #    self._pos_agent=[1,1]
             
         print "reset mode"
         print mode
@@ -98,19 +83,16 @@ class MyEnv(Environment):
             if(self._map[self._pos_agent[0],self._pos_agent[1]+1]==0):
                 self._pos_agent[1]=self._pos_agent[1]+1
         
+        # There is no reward in this simple environment
         self.reward = 0
-        #if (self._pos_agent==self._pos_goal):
-        #    self.reward = 1
-        #if (self._pos_agent[1]>=self._size_maze-2 and action==3):
-        #    self.reward = 1 # used to delete those transitions
 
         self._mode_score += self.reward
         return self.reward
 
     def summarizePerformance(self, test_data_set, learning_algo, *args, **kwargs):
-        #print "test_data_set.observations.shape"
-        #print test_data_set.observations()[0][0:1]
-        
+        """ Plot of the low-dimensional representation of the environment built by the model
+        """
+
         for i in range(1):
             all_possib_inp=[]
             self.create_map()
@@ -127,8 +109,6 @@ class MyEnv(Environment):
 
             
             all_possib_inp=np.expand_dims(np.array(all_possib_inp,dtype='float'),axis=1)
-            #print "all_possib_inp[0:2]"
-            #print all_possib_inp[0:2]
             print "all_possib_inp.shape"
             print all_possib_inp.shape
             print all_possib_inp.dtype
@@ -141,15 +121,11 @@ class MyEnv(Environment):
             print "learning_algo.encoder.predict(all_possib_inp)[0:2]"
             print all_possib_abs_states[0:2]
             
-            #print "print test_data_set.observations()[0:2]"
-            #print test_data_set.observations()[0][0:2]
             n=500
             historics=[]
             for i,observ in enumerate(test_data_set.observations()[0][0:n]):
                 historics.append(np.expand_dims(observ,axis=0))
             historics=np.array(historics)
-            #print "historics[0:2]"
-            #print historics[0:2]
             print "historics.shape"
             print historics.shape
             print historics.dtype
@@ -187,11 +163,7 @@ class MyEnv(Environment):
             y = np.array(abs_states)[:,1]
             if(self.intern_dim>2):
                 z = np.array(abs_states)[:,2]
-            
-            #Colors
-            #onehot_actions = np.zeros((n, 4))
-            #onehot_actions[np.arange(n), actions] = 1
-            
+                        
             fig = plt.figure()
             if(self.intern_dim==2):
                 ax = fig.add_subplot(111)
@@ -202,13 +174,7 @@ class MyEnv(Environment):
                 ax.set_xlabel(r'$X_1$')
                 ax.set_ylabel(r'$X_2$')
                 ax.set_zlabel(r'$X_3$')
-            
-            #for j in range(3):
-            #    # Plot the trajectory
-            #    for i in xrange(n-1):
-            #        #ax.plot(x[j*24+i:j*24+i+2], y[j*24+i:j*24+i+2], z[j*24+i:j*24+i+2], color=plt.cm.cool(255*i/n), alpha=0.5)
-            #        ax.plot(x[j*24+i:j*24+i+2], y[j*24+i:j*24+i+2], color=plt.cm.cool(255*i/n), alpha=0.5)
-            
+                        
             # Plot the estimated transitions
             for i in range(n-1):
                 predicted1=learning_algo.transition.predict([abs_states[i:i+1],np.array([[1,0,0,0]])])
@@ -224,32 +190,7 @@ class MyEnv(Environment):
                     ax.plot(np.concatenate([x[i:i+1],predicted1[0,:1]]), np.concatenate([y[i:i+1],predicted1[0,1:2]]), np.concatenate([z[i:i+1],predicted1[0,2:3]]), color="0.9", alpha=0.75)
                     ax.plot(np.concatenate([x[i:i+1],predicted2[0,:1]]), np.concatenate([y[i:i+1],predicted2[0,1:2]]), np.concatenate([z[i:i+1],predicted2[0,2:3]]), color="0.65", alpha=0.75)
                     ax.plot(np.concatenate([x[i:i+1],predicted3[0,:1]]), np.concatenate([y[i:i+1],predicted3[0,1:2]]), np.concatenate([z[i:i+1],predicted3[0,2:3]]), color="0.4", alpha=0.75)
-                    ax.plot(np.concatenate([x[i:i+1],predicted4[0,:1]]), np.concatenate([y[i:i+1],predicted4[0,1:2]]), np.concatenate([z[i:i+1],predicted4[0,2:3]]), color="0.15", alpha=0.75)
-            
-#            for xx in np.arange(self._size_maze)-self._size_maze//2:
-#                for yy in np.arange(self._size_maze)-self._size_maze//2:
-#                    for zz in np.arange(self._size_maze)-self._size_maze//2:
-#                        predicted1=learning_algo.transition.predict([np.array([[xx,yy,zz]]),np.array([[1,0]])])
-#                        predicted2=learning_algo.transition.predict([np.array([[xx,yy,zz]]),np.array([[0,1]])])
-#                        ax.plot(np.concatenate([np.array([xx]),predicted1[0,:1]]), np.concatenate([np.array([yy]),predicted1[0,1:2]]), np.concatenate([np.array([zz]),predicted1[0,2:]]), color="1", alpha=0.5)
-#                        ax.plot(np.concatenate([np.array([xx]),predicted2[0,:1]]), np.concatenate([np.array([yy]),predicted2[0,1:2]]), np.concatenate([np.array([zz]),predicted2[0,2:]]), color="0.5", alpha=0.5)
-            
-            
-            ## Plot the colorbar for the trajectory
-            #fig.subplots_adjust(right=0.7)
-            #ax1 = fig.add_axes([0.725, 0.15, 0.025, 0.7])
-            ## Set the colormap and norm to correspond to the data for which the colorbar will be used.
-            #cmap = matplotlib.cm.cool
-            #norm = matplotlib.colors.Normalize(vmin=0, vmax=1)
-            #
-            ## ColorbarBase derives from ScalarMappable and puts a colorbar in a specified axes, so it has 
-            ## everything needed for a standalone colorbar.  There are many more kwargs, but the
-            ## following gives a basic continuous colorbar with ticks and labels.
-            #cb1 = matplotlib.colorbar.ColorbarBase(ax1, cmap=cmap,
-            #                        norm=norm,
-            #                        orientation='vertical')
-            #cb1.set_label('Beginning to end of trajectory')
-            
+                    ax.plot(np.concatenate([x[i:i+1],predicted4[0,:1]]), np.concatenate([y[i:i+1],predicted4[0,1:2]]), np.concatenate([z[i:i+1],predicted4[0,2:3]]), color="0.15", alpha=0.75)            
             
             # Plot the dots at each time step depending on the action taken
             length_block=[[0,18],[18,19],[19,31]]
@@ -258,45 +199,15 @@ class MyEnv(Environment):
                     line3 = ax.scatter(all_possib_abs_states[length_block[i][0]:length_block[i][1],0], all_possib_abs_states[length_block[i][0]:length_block[i][1],1], s=30, marker='x', edgecolors='k', alpha=0.5)
                 else:
                     line3 = ax.scatter(all_possib_abs_states[length_block[i][0]:length_block[i][1],0], all_possib_abs_states[length_block[i][0]:length_block[i][1],1] ,all_possib_abs_states[length_block[i][0]:length_block[i][1],2], s=30, marker='x', depthshade=True, edgecolors='k', alpha=0.5)
-            #line2 = ax.scatter(x, y ,z , c=np.tile(np.expand_dims(1-actions/4.,axis=1),(1,3))-0.125, s=50, marker='o', edgecolors='k', alpha=0.75, depthshade=True)
-            #line2 = ax.scatter(x, y, c=np.tile(np.expand_dims(1-actions/4.,axis=1),(1,3))-0.125, s=50, marker='o', edgecolors='k', alpha=0.75)
+
             if(self.intern_dim==2):
                 axes_lims=[ax.get_xlim(),ax.get_ylim()]
             else:
                 axes_lims=[ax.get_xlim(),ax.get_ylim(),ax.get_zlim()]
             
-            #zrange=axes_lims[2][1]-axes_lims[2][0]
-            
             # Plot the legend for the dots
             from matplotlib.patches import Circle, Rectangle
-            from matplotlib.offsetbox import AnchoredOffsetbox, TextArea, DrawingArea, HPacker
-#            box1 = TextArea(" State representation (action 0, action 1): ", textprops=dict(color="k"))
-#            
-#            box2 = DrawingArea(80, 20, 0, 0)
-#            el1 = Circle((10, 10), 5, fc="0.9", edgecolor="k", alpha=0.75)
-#            el2 = Circle((25, 10), 5, fc="0.65", edgecolor="k", alpha=0.75)
-#            el3 = Circle((40, 10), 5, fc="0.4", edgecolor="k", alpha=0.75)
-#            el4 = Circle((55, 10), 5, fc="0.15", edgecolor="k", alpha=0.75) 
-#            #el3 = Circle((50, 10), 5, fc="0", edgecolor="k") 
-#            box2.add_artist(el1)
-#            box2.add_artist(el2)
-#            box2.add_artist(el3)
-#            box2.add_artist(el4)
-#           
-#           
-#            box = HPacker(children=[box1, box2],
-#                          align="center",
-#                          pad=0, sep=5)
-#            
-#            anchored_box = AnchoredOffsetbox(loc=3,
-#                                             child=box, pad=0.,
-#                                             frameon=True,
-#                                             bbox_to_anchor=(0., 1.07),
-#                                             bbox_transform=ax.transAxes,
-#                                             borderpad=0.,
-#                                             )
-#            ax.add_artist(anchored_box)
-            
+            from matplotlib.offsetbox import AnchoredOffsetbox, TextArea, DrawingArea, HPacker            
             
             # Plot the legend for transition estimates
             box1b = TextArea(" Estimated transitions (action 0, 1, 2 and 3): ", textprops=dict(color="k"))
@@ -324,10 +235,6 @@ class MyEnv(Environment):
             ax.add_artist(anchored_box)
             
             
-            
-            #ax.w_xaxis.set_pane_color((0.99, 0.99, 0.99, 0.99))
-            #ax.w_yaxis.set_pane_color((0.99, 0.99, 0.99, 0.99))
-            #ax.w_zaxis.set_pane_color((0.99, 0.99, 0.99, 0.99))
             #plt.show()
             plt.savefig('fig_base'+str(learning_algo.update_counter)+'.pdf')
 
@@ -478,7 +385,6 @@ class MyEnv(Environment):
         #reward_obs[5,2:4]=0.8
         
         for i in indices_reward:
-            #print self._map[i[0]*6:(i[0]+1)*6:,i[1]*6:(i[1]+1)*6]
             obs[i[0]*6:(i[0]+1)*6:,i[1]*6:(i[1]+1)*6]=reward_obs
 
         print indices_agent
@@ -492,10 +398,14 @@ class MyEnv(Environment):
 
 
     def inTerminalState(self):
-#        if((self._pos_agent[0]<=1 and self._cur_action==0) ):#((self._pos_agent==[4,1] and self._cur_action==1) or (self._pos_agent==[5,2] and (self._cur_action==1 or self._cur_action==2)) or (self._pos_agent==[6,3] and self._cur_action==2))):
-#        #(self._pos_agent[1]>=self._size_maze-2 and self._cur_action==1) ):
+        # Uncomment the following lines to add some cases where the episode terminates.
+        # This is used to show how the environment representation interpret cases where 
+        # part of the environment could not be explored.
+#        if((self._pos_agent[0]<=1 and self._cur_action==0) ):
 #            return True
         return False
+
+        # If there is a goal, then terminates the environment when the goas is reached.
         #if (self._pos_agent==self._pos_goal):
         #    return True
         #else:
